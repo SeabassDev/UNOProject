036554e2c0f5099005ae10b567d300a2b166b4bf
SeabassDev <skaye2@student.rccd.edu>
2021-02-27 13:05:22 -0800

    Initial Commit
    Signed-off-by: SeabassDev <skaye2@student.rccd.edu>

---
+++ main.cpp
@@ -0,0 +1,33 @@
+/* 
+ * File: main.cpp
+ * Author: UNO Project Team
+ * Created on February 27, 2021 at 1:02 PM
+ * Purpose: Launch point for the program.
+ */
+
+// System Libraries
+#include <iostream>     // I/O Library
+using namespace std;
+
+// User Libraries
+
+// Global Constants Only
+
+// Function Prototypes
+
+// Execution of Code Begins Here
+int main(int argc, char** argv) {
+    // Random Number Seed
+    
+    // Variable Declarations
+    
+    // Variable Input/Initialization
+    
+    // Processes/Computations
+    
+    // Output
+    
+    // Terminate Program
+    // Close files, deallocate memory, etc...
+    return 0;
+}
